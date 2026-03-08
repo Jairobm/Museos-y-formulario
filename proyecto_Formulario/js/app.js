10bce4c4cb5b1190b33d0dac8f57df532547362e
jairova31 <117950649+Jairobm@users.noreply.github.com>
2026-03-08 22:45:01 +0100

    Add files via upload

---
+++ proyecto_Formulario/js/app.js
@@ -0,0 +1,32 @@
+const form = document.getElementById("formUsuario");
+
+form.addEventListener("submit", function(e){
+    e.preventDefault(); // evita recargar la página
+
+    const nombre = document.getElementById("nombre").value;
+    const apellido1 = document.getElementById("apellido1").value;
+    const apellido2 = document.getElementById("apellido2").value;
+    const fechaNacimiento = document.getElementById("FechaNacimiento").value;
+    const email = document.getElementById("email").value;
+    const password1 = document.getElementById("password1").value;
+    const password2 = document.getElementById("password2").value;
+    // 1) Comprobar longitud mínima
+    if (password1.length < 8) {
+        alert("Error en el campo 'Contraseña': debe tener al menos 8 caracteres.");
+        return;
+    }
+    // 2) Comprobar que coinciden
+    if (password1 !== password2) {
+        alert("Error en el campo 'Repetir contraseña': las contraseñas no coinciden.");
+        return;
+    }
+    // 3) Todo correcto: mostrar mensaje y cambiar color de fondo
+    alert(
+        `Se ha creado correctamente la cuenta de\n` +
+        `${nombre} ${apellido1}`
+    );
+    // Cambiar color de fondo de la página
+    document.body.style.backgroundColor = "#C8E6C9"; // verde suave, pon el color que quieras
+    // Limpiar el formulario
+    form.reset();
+});
